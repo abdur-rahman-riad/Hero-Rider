f40a5c3cac33d1a3716907972d588fd954c76a4d
rahman-riad <rahmanriad.cse@gmail.com>
2021-12-31 22:08:44 +0600

    Header and Footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import logo from '../../../image/logo.PNG';
+
+const Footer = () => {
+    return (
+        <div className="bg-light pt-5 pb-2 mt-5">
+            <Container>
+                <div className="row gy-5 align-items-center">
+                    <div className="col-md-4 text-center">
+                        <img src={logo} width="180px" height="50px" alt="" />
+                    </div>
+
+                    <div className="col-md-4 text-center">
+                        <h5>Important Link</h5>
+                        <li style={{ listStyle: "none" }}>Home</li>
+                        <li style={{ listStyle: "none" }}>About Us</li>
+                        <li style={{ listStyle: "none" }}>FAQ</li>
+                        <li style={{ listStyle: "none" }}>Contact</li>
+                    </div>
+
+                    <div className="col-md-4 text-center">
+                        <h5>Social Media</h5>
+                        <li style={{ listStyle: "none" }}>Facebook</li>
+                        <li style={{ listStyle: "none" }}>Instagram</li>
+                        <li style={{ listStyle: "none" }}>Twitter</li>
+                        <li style={{ listStyle: "none" }}>
+                            <a href="https://rahmanriad.netlify.app/"
+                                target="_blank"
+                                rel="noreferrer"
+                                style={{ textDecoration: "none", color: "black" }}
+                                role="button">Developer</a></li>
+                    </div>
+                </div>
+                <hr />
+                <p className="text-center text-secondary pb-2">CopyRight &copy; All Right Reserved Hero Rider || Design & Developed by
+                    <a href="https://www.linkedin.com/in/rahmanriad/"
+                        target="_blank"
+                        rel="noreferrer"
+                        style={{ textDecoration: "none", color: "black" }}
+                        role="button"> Riad Abdur Rahman</a> </p>
+            </Container>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../image/logo.PNG';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar className="shadow-sm" sticky="top" bg="light" variant="light" collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo}
+                            width="120"
+                            height="40"
+                            style={{ borderRadius: "5px" }}
+                            alt="" />
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle />
+
+                    <Navbar.Collapse className="justify-content-end">
+
+                        <Nav.Link className="fw-bold text-dark" as={Link} to="/">Home</Nav.Link>
+
+                        {/* {user?.email ?
+                            <>
+                                <Nav.Link className="fw-bold text-success" as={Link} to="/dashboard">Dashboard</Nav.Link>
+
+                                <Nav.Link className="fw-bold text-danger" onClick={logOut}>Logout</Nav.Link>
+
+                                <Navbar.Text className="ms-3 d-flex mt-2">
+                                    <img src={avatar}
+                                        width="28"
+                                        height="28" alt="" />
+                                    <h6 className="mt-1 ms-2">{user?.displayName ? user?.displayName : user?.email}</h6>
+                                </Navbar.Text></>
+                            :
+                            <Nav.Link className="fw-bold text-success" as={Link} to="/login">Login</Nav.Link>
+                        } */}
+                        <Nav.Link className="fw-bold text-dark" as={Link} to="/login">Login</Nav.Link>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
